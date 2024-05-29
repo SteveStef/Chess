5da575f6bea62a34a970b7987f880a7956dd5419
Stephen Stefanatos <stephenstef456@gmail.com>
2024-05-29 12:33:30 -0400

    Chess movement

--- server/main.go
+++ server/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
- "github.com/gin-gonic/gin"
- "github.com/rs/cors"
-	. "server/utils"
+  "fmt"
+  "net/http"
+  "github.com/gin-gonic/gin"
+  "github.com/rs/cors"
+  . "server/utils"
 )
 
 
@@ -25,23 +25,26 @@ var Constants BitboardConstants
 
 func Moves(context *gin.Context) {
   var move MoveReq
+
   if err := context.BindJSON(&move); err != nil {
     fmt.Println("Invalid request body")
     fmt.Println(err)
     return
   }
 
-  // what is the piece your trying to move
-  // generate movements for that piece
-  // continue...
-
   intPos := positionFromRowCol(move.Rank, move.File)
-  validMoves := GetValidMoves(intPos, &bitboard, &Constants)
+
+  fmt.Println("Piece:", move.Piece)
+  //DisplayPieceLocation(intPos)
+
+  validMoves := GetValidMoves(move.Piece, intPos, &bitboard, &Constants)
+
   moveList := make([]MoveRes, len(validMoves))
   for i, pos := range validMoves {
     row, col := rowColFromPosition(pos)
     moveList[i] = MoveRes{Rank: row, File: col} 
   }
+
   context.IndentedJSON(http.StatusOK, moveList) 
 }
 
@@ -53,15 +56,15 @@ func positionFromRowCol(row, col uint8) uint64 {
 }
 
 func rowColFromPosition(pos uint64) (row, col uint8) {
-   var r, c uint8
-   for r = 0; r <= 8; r++ {
-       for c = 0; c <= 8; c++ {
-           if ((pos >> (64 - (8 * r + 8 - c))) & 1) == 1 {
-               return r, c
-           }
-       }
-   }
-   return 0, 0
+  var r, c uint8
+  for r = 0; r <= 8; r++ {
+    for c = 0; c <= 8; c++ {
+      if ((pos >> (64 - (8 * r + 8 - c))) & 1) == 1 {
+        return r, c
+      }
+    }
+  }
+  return 0, 0
 }
 
 
@@ -78,9 +81,10 @@ func main() {
   InitBoard(&bitboard)
   PrintBoard(&bitboard)
 
- router := gin.Default()
- router.POST("/moves", Moves)
- handler := cors.Default().Handler(router)
+  router := gin.Default()
+  router.POST("/moves", Moves)
+  handler := cors.Default().Handler(router)
 
- http.ListenAndServe("localhost:8080", handler)
+  http.ListenAndServe("localhost:8080", handler)
 }
+
--- server/utils/chess.go
+++ server/utils/chess.go
@@ -1,24 +1,21 @@
 package utils 
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type BitboardConstants struct {
-   NotAFile uint64
-   NotBFile uint64
-   NotGFile uint64
-   NotHFile uint64
-   NotABFile uint64
-   NotGHFile uint64
+  NotAFile uint64
+  NotBFile uint64
+  NotGFile uint64
+  NotHFile uint64
+  NotABFile uint64
+  NotGHFile uint64
 }
 
 type Bitboard struct {
-	whitePawns, whiteKnights, whiteBishops, whiteRooks, whiteQueens, whiteKing uint64
-	blackPawns, blackKnights, blackBishops, blackRooks, blackQueens, blackKing uint64
+  whitePawns, whiteKnights, whiteBishops, whiteRooks, whiteQueens, whiteKing uint64
+  blackPawns, blackKnights, blackBishops, blackRooks, blackQueens, blackKing uint64
   whiteTurn bool
   castlingRights uint64
-  elPassantSquare uint64
 }
 
 
@@ -57,7 +54,7 @@ func PrintBoard(bitboard *Bitboard) {
             break
           }
         }
-      }
+    }
     }
     fmt.Println()
   }
@@ -65,41 +62,60 @@ func PrintBoard(bitboard *Bitboard) {
 }
 
 func InitBoard(bitboard *Bitboard) {
-	bitboard.whitePawns = uint64(0xFF) << 48
-	bitboard.blackPawns = uint64(0xFF) << 8
-	bitboard.whiteKnights = uint64(0x42) << 56
-	bitboard.blackKnights = uint64(0x42)
-	bitboard.whiteBishops = uint64(0x24) << 56
-	bitboard.blackBishops = uint64(0x24)
-	bitboard.whiteRooks = uint64(0x81) << 56
-	bitboard.blackRooks = uint64(0x81)
-	bitboard.whiteQueens = uint64(0x08) << 56
-	bitboard.blackQueens = uint64(0x08)
-	bitboard.whiteKing = uint64(0x10) << 56
-	bitboard.blackKing = uint64(0x10)
+  bitboard.whitePawns = uint64(0xFF) << 48
+  bitboard.blackPawns = uint64(0xFF) << 8
+  bitboard.whiteKnights = uint64(0x42) << 56
+  bitboard.blackKnights = uint64(0x42)
+  bitboard.whiteBishops = uint64(0x24) << 56
+  bitboard.blackBishops = uint64(0x24)
+  bitboard.whiteRooks = uint64(0x81) << 56
+  bitboard.blackRooks = uint64(0x81)
+  bitboard.whiteQueens = uint64(0x08) << 56
+  bitboard.blackQueens = uint64(0x08)
+  bitboard.whiteKing = uint64(0x10) << 56
+  bitboard.blackKing = uint64(0x10)
   bitboard.whiteTurn = true;
 }
 
 
-func D(piece uint64) {
-	for i := 0; i < 8; i++ {
-		for j := 0; j < 8; j++ {
-			bit := (piece >> (i*8 + j)) & 1
-			switch bit {
-			case 0:
-				fmt.Print(" . ")
-			case 1:
-				fmt.Print(" X ")
-			}
-		}
-		fmt.Println()
-	}
+func DisplayPieceLocation(piece uint64) {
+  for i := 0; i < 8; i++ {
+    for j := 0; j < 8; j++ {
+      bit := (piece >> (i*8 + j)) & 1
+      switch bit {
+      case 0:
+        fmt.Print(" . ")
+      case 1:
+        fmt.Print(" X ")
+    }
+    }
+    fmt.Println()
+  }
   fmt.Println()
 }
 
-func GetValidMoves(knightPosition uint64, bitboard *Bitboard, Constants *BitboardConstants) []uint64 {
+func GetValidMoves(typeOfPiece string, piece uint64, bitboard *Bitboard, Constants *BitboardConstants) []uint64 {
+  if typeOfPiece == "wn" || typeOfPiece == "bn" {
+    return GetKnightMoves(piece, bitboard, Constants, typeOfPiece == "wn")
+  } else if typeOfPiece == "wp" || typeOfPiece == "bp" {
+    //                                              we on the bottom
+    return GetPawnMoves(piece, bitboard, Constants, typeOfPiece == "wp", typeOfPiece == "wp")
+  } else if typeOfPiece == "wr" || typeOfPiece == "br" {
+    return GetRookMoves(piece, bitboard, Constants, typeOfPiece == "wr")
+  }
+  return []uint64{}
+}
+
+func GetKnightMoves(knightPosition uint64, bitboard *Bitboard, Constants *BitboardConstants, isWhite bool) []uint64 {
   var moves []uint64
-  allPieces := bitboard.whitePawns | bitboard.blackPawns | bitboard.whiteKnights | bitboard.blackKnights | bitboard.whiteBishops | bitboard.blackBishops | bitboard.whiteRooks | bitboard.blackRooks | bitboard.whiteQueens | bitboard.blackQueens | bitboard.whiteKing | bitboard.blackKing
+  var allPieces uint64
+
+  if isWhite {
+    allPieces = bitboard.whitePawns | bitboard.whiteKnights | bitboard.whiteBishops | bitboard.whiteRooks | bitboard.whiteQueens | bitboard.whiteKing
+  } else {
+    allPieces = bitboard.blackPawns | bitboard.blackKnights | bitboard.blackBishops | bitboard.blackRooks | bitboard.blackQueens | bitboard.blackKing
+  }
+
   shiftAmounts := []uint64{ 15, 17, 10, 6 }
 
   // Helper function for shifting and appending moves
@@ -121,7 +137,7 @@ func GetValidMoves(knightPosition uint64, bitboard *Bitboard, Constants *Bitboar
       appendIfValid(func(pos uint64) uint64 { return pos << shiftAmount }, shiftAmount, Constants.NotHFile)
     case 6:
       appendIfValid(func(pos uint64) uint64 { return pos << shiftAmount }, shiftAmount, Constants.NotGHFile)
-    }
+  }
   }
 
   // upshifting
@@ -135,8 +151,96 @@ func GetValidMoves(knightPosition uint64, bitboard *Bitboard, Constants *Bitboar
       appendIfValid(func(pos uint64) uint64 { return pos >> shiftAmount }, shiftAmount, Constants.NotAFile)
     case 6:
       appendIfValid(func(pos uint64) uint64 { return pos >> shiftAmount }, shiftAmount, Constants.NotABFile)
+  }
+  }
+
+  return moves
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func GetPawnMoves(pawnPosition uint64, bitboard *Bitboard, Constants *BitboardConstants, onBottom bool, isWhite bool) []uint64 {
+  var moves []uint64
+  var allPieces uint64
+  shiftAmount := uint64(8)
+
+  whitePieces := bitboard.whitePawns | bitboard.whiteKnights | bitboard.whiteBishops | bitboard.whiteRooks | bitboard.whiteQueens | bitboard.whiteKing
+  blackPieces := bitboard.blackPawns | bitboard.blackKnights | bitboard.blackBishops | bitboard.blackRooks | bitboard.blackQueens | bitboard.blackKing
+  allPieces = whitePieces | blackPieces
+  DisplayPieceLocation(whitePieces)
+
+  if onBottom && ((pawnPosition >> shiftAmount) & allPieces) == 0 {
+    moves = append(moves, pawnPosition >> shiftAmount)
+
+  } else if !onBottom && ((pawnPosition << shiftAmount) & allPieces) == 0 {
+    moves = append(moves, pawnPosition << shiftAmount)
+  }
+
+  // double move if pawn hasn't moved
+  if onBottom && !pawnHasMoved(pawnPosition, onBottom) && len(moves) > 0 {
+    moves = append(moves, pawnPosition >> 16)
+  } else if !onBottom && !pawnHasMoved(pawnPosition, onBottom) && len(moves) > 0 {
+    moves = append(moves, pawnPosition << 16)
+  }
+
+  return moves
+}
+
+func pawnHasMoved(pawnPosition uint64, onBottom bool) bool {
+  var initialPawnPosition uint64
+  if onBottom {
+    initialPawnPosition = uint64(0xFF) << 48
+  } else {
+    initialPawnPosition = uint64(0xFF) << 8
+  }
+  result := initialPawnPosition & pawnPosition
+  return result == 0
+}
+
+
+
+
+
+
+
+
+
+func GetRookMoves(rookPosition uint64, bitboard *Bitboard, Constants *BitboardConstants, isWhite bool) []uint64 {
+  var moves []uint64
+  //var allPieces uint64
+  blackPieces := bitboard.blackPawns | bitboard.blackKnights | bitboard.blackBishops | bitboard.blackRooks | bitboard.blackQueens | bitboard.blackKing
+  whitePieces := bitboard.whitePawns | bitboard.whiteKnights | bitboard.whiteBishops | bitboard.whiteRooks | bitboard.whiteQueens | bitboard.whiteKing
+  //allPieces = blackPieces | whitePieces
+
+  // go up
+  for i := 1; i < 8; i++ {
+    shiftedPosition := rookPosition >> (8 * i)
+    // find a way to check if the position is on the board
+    if isWhite && ((shiftedPosition & blackPieces) != 0 || (shiftedPosition == 0)) {
+      moves = append(moves, shiftedPosition)
+      if shiftedPosition & blackPieces != 0 {
+        break
+      }
+    } else if !isWhite && ((shiftedPosition & whitePieces) != 0 || (shiftedPosition == 0)) {
+      moves = append(moves, shiftedPosition)
+      if shiftedPosition & whitePieces != 0 {
+        break
+      }
     }
   }
 
   return moves
 }
+
+
